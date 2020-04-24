34b5a358348f787065491c48e24ea341b9301895
Jepsiko <bibouda512@hotmail.com>
2020-04-24 21:09:26 +0200

    GUI added to the mouse for adding points

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ if __name__ == "__main__":
 	# Taxi taxi
 	taxi = Car("car.png", 400, 300)
 
+	# Level creation
+	points = []
+
 	# Game Loop
 	running = True
 	while running:
@@ -29,7 +32,12 @@ if __name__ == "__main__":
 		screen.fill((0, 50, 0))
 
 		# Draw the taxi
-		taxi.draw(screen)
+		# taxi.draw(screen)
+
+		# Level editing GUI
+		pygame.draw.circle(screen, (150, 200, 150, 10), pygame.mouse.get_pos(), 10)
+		for position in points:
+			pygame.draw.circle(screen, (200, 200, 200, 10), position, 50)
 
 		# Always update the display at the end of the loop
 		pygame.display.update()
