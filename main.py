2260ce7243c8789cf841675d6d838fc410f91932
Jepsiko <bibouda512@hotmail.com>
2020-04-24 14:00:27 +0200

    Close the window event added

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
 
 if __name__ == "__main__":
+	# Initialize pygame
 	pygame.init()
 
+	# Create the screen
 	screen = pygame.display.set_mode((800, 640))
 
-	while True:
-		pass
+	running = True
+	while running:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
