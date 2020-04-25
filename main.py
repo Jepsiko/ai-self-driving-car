be5efe5f31c76a4a998cf3f2017dc57fdb9e3edf
Jepsiko <bibouda512@hotmail.com>
2020-04-25 11:16:29 +0200

    We cannot cross roads and we can remove points

--- main.py
+++ main.py
@@ -7,6 +7,32 @@ HEIGHT = 768
 MIN_DIST_POINTS = 150
 
 
+def draw_line(screen, line, color, width):
+	pos1, pos2 = line
+	x1, y1 = pos1
+	x2, y2 = pos2
+	length = math.hypot(x1 - x2, y1 - y2)
+	angle = math.acos((x2-x1)/length)
+	if y2 < y1:
+		angle = math.pi*2 - angle
+	point1 = (x1 + int(width/2 * math.cos(angle+math.pi/2)), y1 + int(width/2 * math.sin(angle+math.pi/2)))
+	point2 = (x1 + int(width/2 * math.cos(angle-math.pi/2)), y1 + int(width/2 * math.sin(angle-math.pi/2)))
+	point3 = (x2 + int(width/2 * math.cos(angle-math.pi/2)), y2 + int(width/2 * math.sin(angle-math.pi/2)))
+	point4 = (x2 + int(width/2 * math.cos(angle+math.pi/2)), y2 + int(width/2 * math.sin(angle+math.pi/2)))
+	pygame.draw.polygon(screen, color, [point1, point2, point3, point4])
+
+
+def ccw(A, B, C):
+	return (C[1] - A[1]) * (B[0] - A[0]) > (B[1] - A[1]) * (C[0] - A[0])
+
+
+def is_line_crossing(line1, line2):
+	pos1, pos2 = line1
+	pos3, pos4 = line2
+
+	return ccw(pos1, pos3, pos4) != ccw(pos2, pos3, pos4) and ccw(pos1, pos2, pos3) != ccw(pos1, pos2, pos4)
+
+
 if __name__ == "__main__":
 	# Initialize pygame
 	pygame.init()
@@ -27,6 +53,7 @@ if __name__ == "__main__":
 	lineEditing = False
 	startingPoint = None
 	endingPoint = None
+	crossing = False
 	points = []
 	lines = []
 
@@ -42,15 +69,41 @@ if __name__ == "__main__":
 				running = False
 
 			if event.type == pygame.MOUSEBUTTONDOWN:
+
 				if event.button == 1:
 					if lineEditing:
+
+						# Line creation
 						mouseX, mouseY = pygame.mouse.get_pos()
 						for position in points:
-							if math.hypot(position[0] - mouseX, position[1] - mouseY) <= 20:
+							if math.hypot(position[0] - mouseX, position[1] - mouseY) <= 40:
 								if startingPoint is None:
 									startingPoint = position
-								elif position != startingPoint:
+								elif position != startingPoint and not crossing:
 									endingPoint = position
+
+				if event.button == 3:
+
+					# Point removing
+					if pointEditing:
+						mouseX, mouseY = pygame.mouse.get_pos()
+						position_to_remove = None
+						for position in points:
+							if math.hypot(position[0] - mouseX, position[1] - mouseY) <= 40:
+								removePos = position
+								position_to_remove = position
+								break
+
+						# Remove the point and all lines connected to it
+						if position_to_remove is not None:
+							points.remove(position_to_remove)
+							for line in reversed(lines):
+								if position_to_remove in line:
+									lines.remove(line)
+
+					# Cancel the new line drawing
+					if lineEditing:
+						startingPoint = None
 
 			# If keyboard key is pressed
 			if event.type == pygame.KEYDOWN:
@@ -65,8 +118,10 @@ if __name__ == "__main__":
 					lineEditing = True
 					pointEditing = False
 
-				# Line editing mode
+				# Finished mode
 				if event.key == pygame.K_f:
+					taxi.position[0], taxi.position[1] = points[0]
+
 					lineEditing = False
 					pointEditing = False
 
@@ -80,13 +135,12 @@ if __name__ == "__main__":
 
 				spaceAvailable = True
 				for position in points:
-
-					pygame.draw.circle(screen, (100, 150, 255), position, 10)
 					radius = MIN_DIST_POINTS
 					transparent_circle = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
 					pygame.draw.circle(transparent_circle, (150, 150, 150, 50), (radius*2, radius*2), radius-2)
 					pygame.draw.circle(transparent_circle, (200, 200, 200, 50), (radius*2, radius*2), radius, 2)
 					screen.blit(transparent_circle, (position[0] - radius*2, position[1] - radius*2))
+					pygame.draw.circle(screen, (100, 150, 255), position, 10)
 
 					if math.hypot(position[0] - mouseX, position[1] - mouseY) <= MIN_DIST_POINTS:
 						spaceAvailable = False
@@ -98,39 +152,50 @@ if __name__ == "__main__":
 					pygame.draw.circle(screen, (150, 100, 100), (mouseX, mouseY), 8)
 					pygame.draw.circle(screen, (200, 150, 150), (mouseX, mouseY), 10, 2)
 
+				# Add a new point
 				if pygame.mouse.get_pressed()[0] and spaceAvailable:
 					points.append((mouseX, mouseY))
 
 		elif lineEditing:
 			for position in points:
-				pygame.draw.circle(screen, (100, 150, 255), position, 10)
-				radius = int(MIN_DIST_POINTS/4)
+				radius = 30
 				transparent_circle = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
 				pygame.draw.circle(transparent_circle, (150, 150, 150, 50), (radius, radius), radius-2)
 				pygame.draw.circle(transparent_circle, (200, 200, 200, 50), (radius, radius), radius, 2)
 				screen.blit(transparent_circle, (position[0] - radius, position[1] - radius))
+				pygame.draw.circle(screen, (100, 100, 100), position, 10)
+
+			crossing = False
+			for line in lines:
+				draw_line(screen, line, (100, 100, 100), 20)
+
+				if startingPoint is not None and is_line_crossing(line, (startingPoint, (mouseX, mouseY))):
+					crossing = True
 
 			if startingPoint is not None:
-				pygame.draw.line(screen, (100, 100, 100), startingPoint, (mouseX, mouseY), 20)
+				line_to_mouse = (startingPoint, (mouseX, mouseY))
+				if crossing:
+					draw_line(screen, line_to_mouse, (150, 100, 100), 20)
+				else:
+					draw_line(screen, line_to_mouse, (100, 100, 100), 20)
 
+			# Add a new line
 			if endingPoint is not None:
-
 				lines.append([startingPoint, endingPoint])
 
 				startingPoint = None
 				endingPoint = None
 
-			for line in lines:
-				pygame.draw.line(screen, (100, 100, 100), line[0], line[1], 20)
-
 		else:
-			roadWidth = 70
+			# Draw the map
+			roadColor = (100, 100, 100)
+			roadWidth = 60
 			radius = int(roadWidth/2)
 			for position in points:
-				pygame.draw.circle(screen, (100, 100, 100), position, radius)
+				pygame.draw.circle(screen, roadColor, position, radius)
 
 			for line in lines:
-				pygame.draw.line(screen, (100, 100, 100), line[0], line[1], roadWidth)
+				draw_line(screen, line, roadColor, roadWidth)
 
 			# Draw the taxi
 			taxi.draw(screen)
