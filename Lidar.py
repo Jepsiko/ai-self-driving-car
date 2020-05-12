4af6307d126dddbdd9c11a675fb5b2abe8fc0a59
Arthur Pierrot <arthur.pierrot@hotmail.com>
2020-05-12 16:17:48 +0200

    Better code

--- Lidar.py
+++ Lidar.py
@@ -4,6 +4,10 @@ import numpy as np
 from pygame.math import Vector2
 
 
+def get_point_at_vector(pos, magnitude, angle):
+	return Vector2(pos.x + magnitude * math.cos(angle), pos.y + magnitude * math.sin(angle))
+
+
 class Lidar:
 
 	def __init__(self, lin=5, col=5, width=200, length=150, back_length=50):
@@ -24,19 +28,13 @@ class Lidar:
 
 	def draw(self, screen, pos, angle):
 
-		front = Vector2(pos.x + (self.length - self.back_length) * math.cos(angle),
-						pos.y + (self.length - self.back_length) * math.sin(angle))
-		front_left = Vector2(front.x + self.width / 2 * math.cos(math.pi / 2 + angle),
-							 front.y + self.width / 2 * math.sin(math.pi / 2 + angle))
-		front_right = Vector2(front.x + self.width / 2 * math.cos(angle - math.pi / 2),
-							  front.y + self.width / 2 * math.sin(angle - math.pi / 2))
+		front = get_point_at_vector(pos, self.length - self.back_length, angle)
+		front_left = get_point_at_vector(front, self.width / 2, math.pi / 2 + angle)
+		front_right = get_point_at_vector(front, self.width / 2, angle - math.pi / 2)
 
-		back = Vector2(pos.x + self.back_length * math.cos(math.pi + angle),
-					   pos.y + self.back_length * math.sin(math.pi + angle))
-		back_left = Vector2(back.x + self.width / 2 * math.cos(math.pi / 2 + angle),
-					 		back.y + self.width / 2 * math.sin(math.pi / 2 + angle))
-		back_right = Vector2(back.x + self.width / 2 * math.cos(angle - math.pi / 2),
-							 back.y + self.width / 2 * math.sin(angle - math.pi / 2))
+		back = get_point_at_vector(pos, self.back_length, math.pi + angle)
+		back_left = get_point_at_vector(back, self.width / 2, math.pi / 2 + angle)
+		back_right = get_point_at_vector(back, self.width / 2, angle - math.pi / 2)
 
 		lidar_points = [front_left, back_left, back_right, front_right]
 		pygame.draw.lines(screen, (200, 0, 0), True, lidar_points)
