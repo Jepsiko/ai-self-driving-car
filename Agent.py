2f68d17fa805321163ebc65cf3598294521d7c41
Arthur Pierrot <arthur.pierrot@hotmail.com>
2020-05-22 17:52:37 +0200

    Code running

--- Agent.py
+++ Agent.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
-import tensorflow as tf
+# noinspection PyUnresolvedReferences
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 
 class OUActionNoise(object):
@@ -37,7 +39,7 @@ class ReplayBuffer(object):
 		self.state_memory[index] = state
 		self.new_state_memory[index] = state_
 		self.reward_memory[index] = reward
-		self.action_memory = action
+		self.action_memory[index] = action
 		self.terminal_memory[index] = 1 - int(done)
 		self.mem_cntr += 1
 
@@ -54,7 +56,7 @@ class ReplayBuffer(object):
 		return states, actions, rewards, new_states, terminal
 
 
-RandomUniform = tf.initializers.RandomUniform
+random_uniform = tf.initializers.random_uniform
 
 
 class Actor(object):
@@ -78,34 +80,34 @@ class Actor(object):
 		self.unnormalized_actor_gradients = tf.gradients(self.mu, self.params, self.action_gradient)
 
 		self.actor_gradients = list(map(lambda x: tf.div(x, self.batch_size), self.unnormalized_actor_gradients))
-		self.optimize = tf.train.AdamOptimizer(self.lr).apply_gradients(zip(self.actor_gradients, self.parmas))
+		self.optimize = tf.train.AdamOptimizer(self.lr).apply_gradients(zip(self.actor_gradients, self.params))
 
 	def build_network(self):
 		with tf.variable_scope(self.name):
 			self.input = tf.placeholder(tf.float32, shape=[None, *self.input_dims], name='inputs')
 			self.action_gradient = tf.placeholder(tf.float32, shape=[None, self.n_actions])
 			f1 = 1 / np.sqrt(self.fc1_dims)
-			dense1 = tf.layers.dense(self.input, units=self.fc1_dims, kernel_initializer=RandomUniform(-f1, f1),
-									 bias_initializer=RandomUniform(-f1, f1))
+			dense1 = tf.layers.dense(self.input, units=self.fc1_dims, kernel_initializer=random_uniform(-f1, f1),
+									 bias_initializer=random_uniform(-f1, f1))
 			batch1 = tf.layers.batch_normalization(dense1)
 			layer1_activation = tf.nn.relu(batch1)
 
 			f2 = 1 / np.sqrt(self.fc2_dims)
-			dense2 = tf.layers.dense(layer1_activation, unit=self.fc2_dims, kernel_initializer=RandomUniform(-f2, f2),
-									 bias_initializer=RandomUniform(-f2, f2))
-			batch2 = tf.layers.batch.normalization(dense2)
+			dense2 = tf.layers.dense(layer1_activation, units=self.fc2_dims, kernel_initializer=random_uniform(-f2, f2),
+									 bias_initializer=random_uniform(-f2, f2))
+			batch2 = tf.layers.batch_normalization(dense2)
 			layer2_activation = tf.nn.relu(batch2)
 
 			f3 = 0.003
 			mu = tf.layers.dense(layer2_activation, units=self.n_actions, activation='tanh',
-								 kernel_initializer=RandomUniform(-f3, f3), bias_initializer=RandomUniform(-f3, f3))
+								 kernel_initializer=random_uniform(-f3, f3), bias_initializer=random_uniform(-f3, f3))
 			self.mu = tf.multiply(mu, self.action_bound)
 
 	def predict(self, inputs):
 		return self.sess.run(self.mu, feed_dict={self.input: inputs})
 
 	def train(self, inputs, gradients):
-		self.sess.run(self.optimize, feed_dict={self.inputs: inputs, self.action_gradient: gradients})
+		self.sess.run(self.optimize, feed_dict={self.input: inputs, self.action_gradient: gradients})
 
 	def save_checkpoint(self):
 		print('...saving chackpoint...')
@@ -144,36 +146,36 @@ class Critic(object):
 			self.q_target = tf.placeholder(tf.float32, shape=[None, 1], name='target')
 
 		f1 = 1 / np.sqrt(self.fc1_dims)
-		dense1 = tf.layers.dense(self.input, units=self.fc1_dims, kernel_initializer=RandomUniform(-f1, f1),
-								 bias_initializer=RandomUniform(-f1, f1))
+		dense1 = tf.layers.dense(self.input, units=self.fc1_dims, kernel_initializer=random_uniform(-f1, f1),
+								 bias_initializer=random_uniform(-f1, f1))
 		batch1 = tf.layers.batch_normalization(dense1)
 		layer1_activation = tf.nn.relu(batch1)
 
 		f2 = 1 / np.sqrt(self.fc2_dims)
-		dense2 = tf.layers.dense(layer1_activation, unit=self.fc2_dims, kernel_initializer=RandomUniform(-f2, f2),
-								 bias_initializer=RandomUniform(-f2, f2))
-		batch2 = tf.layers.batch.normalization(dense2)
+		dense2 = tf.layers.dense(layer1_activation, units=self.fc2_dims, kernel_initializer=random_uniform(-f2, f2),
+								 bias_initializer=random_uniform(-f2, f2))
+		batch2 = tf.layers.batch_normalization(dense2)
 
-		action_in = tf.layers.dense(self.action, units=self.fc2_dims, activation='relu')
+		action_in = tf.layers.dense(self.actions, units=self.fc2_dims, activation='relu')
 
 		state_actions = tf.add(batch2, action_in)
 		state_actions = tf.nn.relu(state_actions)
 
 		f3 = 0.003
-		self.q = tf.layers.dense(state_actions, units=1, kernel_initializer=RandomUniform(-f3, f3),
-								 bias_initializer=RandomUniform(-f3, f3),
+		self.q = tf.layers.dense(state_actions, units=1, kernel_initializer=random_uniform(-f3, f3),
+								 bias_initializer=random_uniform(-f3, f3),
 								 kernel_regularizer=tf.keras.regularizers.l2(0.01))
 		self.loss = tf.losses.mean_squared_error(self.q_target, self.q)
 
 	def predict(self, inputs, actions):
-		return self.sess.run(self.q, feel_dict={self.input: inputs, self.actions: actions})
+		return self.sess.run(self.q, feed_dict={self.input: inputs, self.actions: actions})
 
 	def train(self, inputs, actions, q_target):
 		return self.sess.run(self.optimize,
 							 feed_dict={self.input: inputs, self.actions: actions, self.q_target: q_target})
 
 	def get_action_gradient(self, inputs, actions):
-		return self.sess.run(self.action_gradient, feed_fict={self.input: inputs, self.actions: actions})
+		return self.sess.run(self.action_gradients, feed_dict={self.input: inputs, self.actions: actions})
 
 	def save_checkpoint(self):
 		print('...saving chackpoint...')
--- test.py
+++ test.py
@@ -1,16 +1,33 @@
 import Agent
 import gym
 import numpy as np
+# from utils import plotLearning
+
 
 if __name__ == "__main__":
+	using_ai = True
 	env = gym.make("Pendulum-v0")
-	agent = Agent.Agent(alpha=0.0001, beta=0.001, input_dims=[3], tau=0.001, env=env, batch_size=64, layer1_size=400,
+	agent = Agent.Agent(alpha=0.0001, beta=0.001, input_dims=[3], tau=0.0005, env=env, batch_size=64, layer1_size=400,
 						layer2_size=300, n_actions=1)
 	score_history = []
 	np.random.seed(0)
 	for i in range(1000):
-		obs = env.reset()
+		state = env.reset()
 		done = False
 		score = 0
 		while not done:
-			act = agent
+			if using_ai:
+				action = agent.choose_action(state)
+			else:
+				action = float(input("action : "))
+			new_state, reward, done, info = env.step(action)
+			if using_ai:
+				agent.remember(state, action, reward, new_state, int(done))
+				agent.learn()
+			score += reward
+			state = new_state
+		score_history.append(score)
+		print("episode ", i, 'score %.2f' % score, '100 game average %.2f' % np.mean(score_history[-100:]))
+
+	# filename = 'pendulum.png'
+	# plotLearning(score_history, filename, window=100)
