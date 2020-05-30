2485a9da513c7275130eb2ffaf556b4159322fee
thibaut <thibaut.colson1996@gmail.com>
2020-05-30 02:07:29 +0200

    transfert vers arthur

--- main.py
+++ main.py
@@ -9,67 +9,71 @@ def plot_learning_curve(x, scores, figure_file):
 		running_avg[i] = np.mean(scores[max(0, i - 100):(i + 1)])
 	plt.plot(x, running_avg)
 	plt.title('Running average of previous 100 scores')
-	plt.savefig(figure_file, format='eps')
+	plt.savefig(figure_file)
+	plt.show()
 
 
 def main():
-	if input('Enter map editor (y/n) ? ') == 'y':
-		env = Env.Env('Taxi Agent')
-		env.run()
-
-	else:
-		env = Env.Env('Taxi Agent', map_name=input("Map name : "))
-
-		agent = Agent.Agent(alpha=0.0001, beta=0.001, input_dims=[env.get_number_inputs()], tau=0.001, n_actions=2,
-							fc1_dims=400, fc2_dims=300)
+	# if input('Enter map editor (y/n) ? ') == 'y':
+	# 	env = Env.Env('Taxi Agent')
+	# 	env.run()
+	#
+	# else:
+	# 	env = Env.Env('Taxi Agent', map_name=input("Map name : "))
+	#
+	# 	agent = Agent.Agent(alpha=0.0001, beta=0.001, input_dims=[env.get_number_inputs()], tau=0.001, n_actions=2,
+	# 						fc1_dims=400, fc2_dims=300)
+	#
+	# 	score_history = []
+	# 	mean_history = []
+	# 	np.random.seed(0)
+	# 	info = ''
+	# 	i = 0
+	#
+	# 	n_games = int(input('Number of games : '))
+	# 	display = input('Display (y/n) ? ') == 'y'
+	#
+	# 	if input('Load Agent (y/n) ? ') == 'y':
+	# 		agent.load_models(input('Agent name : '))
+	#
+	# 	while info != 'Quit' and i < n_games:
+	#
+	# 		obs = env.reset()
+	# 		done = False
+	# 		score = 0
+	#
+	# 		while not done:
+	# 			# print('\n------------- State  -------------')
+	# 			# print(obs)
+	# 			action = agent.choose_action(obs)
+	# 			# print('\n------------- Action -------------')
+	# 			# print(action)
+	#
+	# 			new_state, reward, done, info = env.step(action)
+	#
+	# 			if display:
+	# 				env.render()
+	#
+	# 			agent.remember(obs, action, reward, new_state, done)
+	# 			agent.learn()
+	#
+	# 			score += reward
+	# 			obs = new_state
+	#
+	# 		score_history.append(score)
+	# 		mean = np.mean(score_history[-100:])
+	# 		mean_history.append(mean)
+	# 		print("episode ", i, 'score %.2f' % score, '100 game average %.2f' % mean)
+	# 		i += 1
+	#
+	# 	if input('Save Agent (y/n) ? ') == 'y':
+	# 		agent.save_models(input('Agent name : '))
 
 		score_history = []
-		mean_history = []
-		np.random.seed(0)
-		info = ''
-		i = 0
-
-		n_games = int(input('Number of games : '))
-		display = input('Display (y/n) ? ') == 'y'
-
-		if input('Load Agent (y/n) ? ') == 'y':
-			agent.load_models(input('Agent name : '))
-
-		while info != 'Quit' and i < n_games:
-
-			obs = env.reset()
-			done = False
-			score = 0
-
-			while not done:
-				# print('\n------------- State  -------------')
-				# print(obs)
-				action = agent.choose_action(obs)
-				# print('\n------------- Action -------------')
-				# print(action)
-
-				new_state, reward, done, info = env.step(action)
-
-				if display:
-					env.render()
-
-				agent.remember(obs, action, reward, new_state, done)
-				agent.learn()
-
-				score += reward
-				obs = new_state
-
-			score_history.append(score)
-			mean = np.mean(score_history[-100:])
-			mean_history.append(mean)
-			print("episode ", i, 'score %.2f' % score, '100 game average %.2f' % mean)
-			i += 1
-
-		if input('Save Agent (y/n) ? ') == 'y':
-			agent.save_models(input('Agent name : '))
-
-		for score in score_history:
-			print(score)
+		file = open('city41_chiffre', 'r')
+		for line in file.readlines():
+			score_history.append(float(line.strip()))
+		n_games = len(score_history)
 		x = [i + 1 for i in range(n_games)]
 		plot_learning_curve(x, score_history, input('Plot file name (.eps) : '))
 
